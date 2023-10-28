e321bd64c8f7f8fae533005061a3c57f7da5dbb0
piyush <piyushchandra9458@gmail.com>
2023-10-28 03:11:36 -1200

    ok

--- src/Loginpage.tsx
+++ src/Loginpage.tsx
@@ -89,7 +89,7 @@ const Loginpage = () => {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Sign in
+                Log in
               </button>
             </div>
           </form>
